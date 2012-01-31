e7c7cde1a250ed5d229c5af5313140032b866167
Tim Triche, Jr <ttriche@usc.edu>
2012-01-31 14:48:41 -0800

    small tweaks to df2GR and GR2df (for use with DB)

--- R/GR2df.R
+++ R/GR2df.R
@@ -3,13 +3,13 @@ getMetadata <- function(x)
   message('Warning: this probably will not work out exactly as you expect...')
   emd = names(elementMetadata(x))
   skip = sapply(emd, function(y) is(elementMetadata(x)[[y]], 'AtomicList'))
-  as.data.frame(lapply(emd[-skip], function(z) unlist(elementMetadata(x)[[z]])))
+  as.data.frame(lapply(emd[!skip], function(z) unlist(elementMetadata(x)[[z]])))
 } # }}}
 
 GR2df <- function(GR, keepColumns=FALSE, ignoreStrand=FALSE) 
 { # {{{
   stopifnot(class(GR) == 'GRanges')
-  tmp = data.frame(chr=as.vector(seqnames(GR)), start=start(GR), end=end(GR))
+  tmp = data.frame(chr=as(seqnames(GR),'vector'), start=start(GR), end=end(GR))
   if(!ignoreStrand) tmp = cbind(tmp, strand=as.vector(strand(GR)))
   if(keepColumns) tmp = cbind(tmp, getMetadata(GR))
   return(tmp)
