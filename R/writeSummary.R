5c8f7549e2bf4f40fc09eb7abe8a504dbeac9672
Moiz Bootwalla <msbootwalla@gmail.com>
2012-03-06 16:33:16 -0800

    Created runSampleQC method and bumped version
    runSampleQC finds samples that fail QC i.e. have >10000 probes with
    p>0.05. Also modified some methods in writeSummary to provide a better
    summary.

--- R/writeSummary.R
+++ R/writeSummary.R
@@ -10,10 +10,9 @@ writeSampleSummary <- function(x, filepath='.'){
 	disease <- unique(x$diseaseabr)[1]
 	pvals <- pvals(x)
 	sample.summary <- getSampleSummary(pvals)
-	ss <- paste(paste(names(sample.summary), sample.summary, sep="\t"), collapse="\n")
-	header <- paste("TCGA.ID", "No. of Failed Probes (p > 0.05)", sep="\t")
+	ss <- data.frame("TCGA.ID"=x$TCGA.ID, "Barcode"=x$barcode, "Batch"=x$TCGA.BATCH, "Num.Failed.Probes"=sample.summary)
 	filename <- paste(disease, "sample", "summary", "txt", sep=".")
-	cat(header, ss, file=file.path(filepath, filename), sep="\n")
+	write.table(ss, file=file.path(filepath, filename), row.names=FALSE, quote=FALSE, sep="\t")
 }
 
 getProbeSummary <- function(pvals)
@@ -47,3 +46,25 @@ plotDensities <- function(object, controls=NULL, label='Replicate betas'){ # {{{
   plot(ds[[1]], main=label, xlim=c(0, 1), ylim=c(0, ymax), xlab='Methylation')
   for(i in seq_along(ds)) lines(ds[[i]])
 } # }}}
+
+runSampleQC <- function(x, filepath='.')
+{
+	disease <- unique(x$diseaseabr)[1]
+	pvals <- pvals(x)
+	sample.summary <- getSampleSummary(pvals)
+	if(any(sample.summary > 10000)){
+		sample.summary <- sample.summary[which(sample.summary > 10000)]
+		failed <- paste(names(sample.summary), collapse=",")
+		index <- which(x$barcode %in% names(sample.summary))
+		fs <- data.frame("TCGA.ID"=x$TCGA.ID[index], "Barcode"=x$barcode[index], "Histology"=x$histology[index],
+				 "Batch"=x$TCGA.BATCH[index], "Num.Failed.Probes"=sample.summary)
+		filename <- paste(disease, "sample", "QC", "txt", sep=".")
+		message(paste("Samples", failed, "failed QC and will be removed"))
+		message(paste("Writing QC log to", file.path(filepath, filename)))
+		write.table(fs, file=file.path(filepath, filename), row.names=FALSE, quote=FALSE, sep="\t")
+		return(index)
+	} else {
+		message("All samples passed QC")
+		return(NULL)
+	}
+}
